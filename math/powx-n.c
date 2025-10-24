4709a83ba5e4adf631b10b5c1918ec6b0791e9f6
NAsihs13 <harryxu0503@gmail.com>
2025-10-25 02:21:30 +1100

    LC-powx-n [Runtime: N/A; Memory: N/A]

--- math/powx-n.c
+++ math/powx-n.c
@@ -1,8 +1,11 @@
 double myPow(double x, int n) {
-    long long n1 = n;
+    long long exp = n;
 
-    if (n1 == 0) return 1;
-    if (n1 < 0) return 1/myPow(x,-n1);
+    if (exp == 0) return 1;
+    if (exp < 0) {
+        x = 1/x;
+        exp = -exp;
+    }
 
-    return x * myPow(x, n1-1);
+    return x * myPow(x, exp-1);
 }
