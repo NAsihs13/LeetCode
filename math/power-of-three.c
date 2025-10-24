2841284292459601167f984a61d9a229be1df757
NAsihs13 <harryxu0503@gmail.com>
2025-10-25 01:12:08 +1100

    LC-power-of-three [Runtime: 0 ms; Memory: 8.4 MB]

--- math/power-of-three.c
+++ math/power-of-three.c
@@ -1,9 +1,7 @@
 bool isPowerOfThree(int n) {
-    if (n <= 0 || n % 3 != 0) {
-        return false;
-    } else {
-        return true;
-    }
+    if (n == 1) return true;
+
+    if (n <= 0 || n % 3 != 0) return false;
 
     return isPowerOfThree(n/3);
 }
