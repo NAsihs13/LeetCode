0b53aae314513c49be12f8e8c5df915f8adda788
NAsihs13 <harryxu0503@gmail.com>
2025-12-23 20:33:38 +0800

    LC-reverse-nodes-in-k-group [Runtime: 0 ms; Memory: 11.5 MB]

--- recursion/reverse-nodes-in-k-group.c
+++ recursion/reverse-nodes-in-k-group.c
@@ -15,15 +15,15 @@ struct ListNode* reverseKGroup(struct ListNode* head, int k) {
         curr = curr->next;
     }
 
-    //
     struct ListNode *prev = NULL;
     curr = head;
 
+    // 翻转k范围内的linked list
     for (int i = 0; i < k; i++) {
         struct ListNode *next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
+        curr->next = prev;                      // 把当前节点指向前一个节点
+        prev = curr;                            // 指向翻转好部分的头
+        curr = next;                            // 去处理下一个没有反转好的节点
     }
 
     head->next = reverseKGroup(curr, k);
