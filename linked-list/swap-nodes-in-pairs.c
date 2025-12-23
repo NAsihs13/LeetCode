d1b62500e1d0af1d2fd7ae8de1639bbfd678bb3f
NAsihs13 <harryxu0503@gmail.com>
2025-12-23 17:55:32 +0800

    LC-swap-nodes-in-pairs [Runtime: 0 ms; Memory: 9 MB]

---
+++ linked-list/swap-nodes-in-pairs.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* swapPairs(struct ListNode* head) {
+    if(head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    struct ListNode *n1 = head;
+    struct ListNode *n2 = head->next;
+
+    struct ListNode *rest = swapPairs(n2->next);
+
+    n2->next = n1;
+    n1->next = rest;
+
+    return n2;
+}
