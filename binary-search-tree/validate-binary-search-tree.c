635585876aebcc51dd90e2cb565c121a935700c2
NAsihs13 <harryxu0503@gmail.com>
2026-01-07 17:00:31 +0800

    LC-validate-binary-search-tree [Runtime: N/A; Memory: N/A]

---
+++ binary-search-tree/validate-binary-search-tree.c
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool bst(struct TreeNode *root, int min, int max) {
+    if (root == NULL) return true;
+
+    if (root->val < min && root->val > max) {
+        return false;
+    }
+
+    return bst(root->left, min, root->val) && bst(root->right, root->val, max);
+
+}
+
+ 
+
+bool isValidBST(struct TreeNode* root) {
+    return bst(root, LONG_MIN, LONG_MAX);
+}
