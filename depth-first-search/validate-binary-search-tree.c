c25c8afc99be391ad2e4eaf67fc294a0e903b90e
NAsihs13 <harryxu0503@gmail.com>
2026-01-07 19:51:36 +0800

    LC-validate-binary-search-tree [Runtime: 0 ms; Memory: 12.1 MB]

--- depth-first-search/validate-binary-search-tree.c
+++ depth-first-search/validate-binary-search-tree.c
@@ -6,22 +6,16 @@
  *     struct TreeNode *right;
  * };
  */
-bool bst(struct TreeNode *root, int min, int max) {
+bool bst(struct TreeNode *root, long min, long max) {
     if (root == NULL) return true;
 
-    if (root->left == NULL && root->right == NULL) return true;
-    if (root->left == NULL || root->right == NULL) return false;
-    
-
-    if (root->val <= min && root->val >= max) {
+    if (root->val <= min || root->val >= max) {
         return false;
     }
 
-    return bst(root->left, min, root->val) && bst(root->right, root->val, max);
-
+    return bst(root->left, min, root->val) &&
+           bst(root->right, root->val, max);
 }
-
- 
 
 bool isValidBST(struct TreeNode* root) {
     return bst(root, LONG_MIN, LONG_MAX);
